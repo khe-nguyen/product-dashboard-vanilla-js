f2375a738ef97f3f36baec37a1165e6941b421e5
Khe Nguyen <khenguyen@usf.edu>
2025-10-19 23:18:35 -0400

    creat function using async/await

--- cp_2.js
+++ cp_2.js
@@ -14,3 +14,14 @@ function fetchProductsThen() {
       console.log("An error occurred:", error);
     });
 }
+
+// Step 4: Using async/await
+async function fetchProductsAsync() {
+  try {
+    const response = await fetch("https://www.course-api.com/javascript-store-products");
+    const products = await response.json();
+    displayProducts(products); // Send data to display function
+  } catch (error) {
+    handleError(error);
+  }
+}
